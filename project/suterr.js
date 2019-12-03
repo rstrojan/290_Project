064ebc8175d93ecc69ce7f520c5320afaa92bf7f
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-03 14:56:04 -0800

    Update suterr.js

--- project/suterr.js
+++ project/suterr.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3469);
+app.set('port', 34691);
 
 app.get('/',function(req,res){
 	res.render('home');
