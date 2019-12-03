a27fbe87a739d93cb955be3d3c9d60c5c12917b5
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-03 15:02:40 -0800

    Update suterr.js

--- project/suterr.js
+++ project/suterr.js
@@ -29,7 +29,7 @@ app.get('/Find_out_more', function(req,res){
 
 app.get('/ticket_print',function(req,res){
   var qParams = [];
-  context.watchthis = req.query.Departure_City;
+  context.watchthis = req.query.Destination;
   for (var p in req.query){
     qParams.push({'name':p,'value':req.query[p]})
   }
